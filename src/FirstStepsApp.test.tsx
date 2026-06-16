57b3bf7304b31be640dc5a7b4c79a79b411bcee6
CrisRD2 <crruizdelgado@gmail.com>
2026-06-16 11:11:11 +0200

    Esperar argumentos especificos

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,16 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import { afterEach, describe, expect, test, vi } from 'vitest';
 import { FirstStepApp } from './FirstStepsApp';
+import { ItemCouter } from './shopping-card/ItemCouter';
 
-const mockItemCouter = vi.fn();
+const mockItemCouter = vi.fn((_props:unknown)=>{
+  return ( <div
+      data-testid="ItemCouter"
+      />)
+});
+
 
 vi.mock('./shopping-card/ItemCouter', () => ({
-  ItemCouter: (props: any) => {
-    mockItemCouter(props);
-    return <div data-testid="ItemCouter" />;
-  },
+  ItemCouter: (props: unknown) => mockItemCouter(props),
 }));
-
+/* vi.mock('./shopping-card/ItemCouter', () => ({
+  ItemCouter: (props: unknown) => {
+    mockItemCouter(props);
+     <div
+      data-testid="ItemCouter"
+      name={props.name}
+       quantity={props.quantity} />; 
+  },
+})); 
+ */
 describe('FirstStepsApp', () => {
   afterEach(() => {
     vi.clearAllMocks();
@@ -32,7 +44,7 @@ describe('FirstStepsApp', () => {
   test('should render ItemCouter with correct props', () => {
     render(<FirstStepApp />);
 
-    expect(mockItemCouter).toHaveBeenCalledTimes(3); // React puede re-renderizar
+    expect(mockItemCouter).toHaveBeenCalledTimes(3); 
 
     expect(mockItemCouter).toHaveBeenCalledWith({
       name: 'Nintendo Switch 2',
